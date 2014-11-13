1b64785cb1ba977244c4412e61f5cecef7a8c665
jakubsindelar <github@heroesofmemory.eu>
2014-11-13 13:25:06 +0100

    šablona

--- wp-content/themes/videno_hmatem/index.php
+++ wp-content/themes/videno_hmatem/index.php
@@ -4,34 +4,18 @@
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 <title>index</title>
 <link href="StyleSheet1.css" rel="stylesheet" type="text/css" />
+<style>
+.main background-color: green;
+</style>
 </head>
 
 <body bgcolor="#333333" leftmargin="0" topmargin="0" >
 
-<div>
-
-<table width="975" border="0"cellpadding="0" cellspacing="0" align="center">
-  <tr>
-    <td><iframe src="Left_header.htm" id="L_header" width="200" height="215"  align="left" scrolling="no" frameborder="0"></iframe>
-        <iframe src="VH_header.htm" id="header" width="775" height="215"  align="left" scrolling=n"o" frameborder=0></iframe>
-    </td>
-    </tr>
-  <tr>
-    <td><iframe src="Left_columm.htm" id="Left_columm" width="200" height="850" align="left" scrolling="no" frameborder="0"></iframe>
-        <iframe src="Novinky.html" id="Obsah" width="560" height="850" align="left" name="obsah"  frameborder="0"></iframe>
-        <iframe src="Right_columm.htm" id="Right_columm" width="215" height="850" align="left" "scrolling="no" frameborder="0"></iframe>
-        </td>
-        </tr>
-  <tr>
-    <td><iframe src="Left_Footer.htm" id="Lfooter" width="200" height="130" align="left" scrolling="no" frameborder="0"></iframe>
-    <iframe src="Footer.htm" id="footer" width="775" height="130"  align="left" scrolling="no" frameborder="0"></iframe>
-    </td>
-    </tr>
-</table>
-
-
+<div id="main">
+<span>Reklama</span>
+<span>Menu</span>
+<span>logo</span>
 </div>
-
 
 </body>
 </html>
@@ -39,4 +23,3 @@
 
 
 
-  
